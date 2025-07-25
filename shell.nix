777515c0fbfc51e659818c4eadb9814c34fc108f
josh <joshcapitals07@gmail.com>
2025-07-25 16:46:23 -0400

    edited nix shell

--- shell.nix
+++ shell.nix
@@ -21,5 +21,8 @@ pkgs.mkShell {
     
     # Optional: set RUST_BACKTRACE for better error messages
     export RUST_BACKTRACE=1
+
+    # prompt
+    export PS1='$(__git_ps1 "\e[1;32m(%s)\e[0m")\e[1;35m \W\e[0m\e[1;37m   \e[0m'
   '';
 }
